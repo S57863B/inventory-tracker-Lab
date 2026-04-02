3e01884530c9a48da85f31902c6406cb53bb046a
S57863B <i349cjhswe@gmail.com>
2026-04-02 00:47:12 -0400

    feat: added base Product class

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,8 +1,17 @@
-export class Product {
+class Product {
     constructor(
         public sku: string,
         public name: string,
         public price: number
     ) {}
 
+    displayDetails(): string {
+        return `[${this.sku}] ${this.name} - ${this.price}`;
+    }
+
+    getPriceWithTax(): number {
+        return this.price;
+    }
 }
+
+export = Product;
